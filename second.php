6a30aba065975f1b1bb7ec8a2a52bb43c46eb5dc
Magdiel <luisc@thehangar.cr>
2014-07-15 10:14:54 -0600

    Adding conn.php

--- second.php
+++ second.php
@@ -1,6 +1,7 @@
 <?
-$mongo = new Mongo('mongodb://luisc:leidgam17@ds061258.mongolab.com:61258/peoplelbees'); // Inicializo la clase de Mongo
-$database = $mongo->selectDB("peoplelbees"); //Selecciono la Base de Datos, si no existe la crea
+
+include('conn.php');
+
 $estudiante = $database->selectCollection('users'); // Selecciono la coleccion (tabla), y la asigno a un objeto
 
 $accion = $_GET['accion'] ? $_GET['accion'] : 'index' ;
