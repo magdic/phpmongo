45f425613d674510cbf7e902ad3e10a480e506fe
Magdiel Cordero <contact@magdielcordero.info>
2015-09-21 12:54:58 -0600

    dot

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php 
 
-	$mongo = new Mongo('mongodb://luisc:leidgam17@ds061258.mongolab.com:61258/peoplelbees'); // Inicializo la clase de Mongo
+	$mongo = new Mongo('mongodb://luiscetup:pass@ds061258.mongolab.com:61258/peoplelbees'); // Inicializo la clase de Mongo
 	$database = $mongo->selectDB("peoplelbees"); //Selecciono la Base de Datos, si no existe la crea
 ?>
